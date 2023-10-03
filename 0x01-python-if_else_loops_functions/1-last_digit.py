feab175b9437e40a8a5323eb5a47bbac3d61515d
ilias <iliaschokairi@gmail.com>
2023-10-03 11:36:24 +0100

    compare the last digit in a random program generator

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
+if number < 0:
+    last_digit = (abs(number) % 10) * -1
+else:
+    last_digit = number % 10
+if last_digit > 5:
     print("Last digit of", end=" ")
     print(number, end=" ")
     print("is", end=" ")
-    print(number % 10, end=" ")
+    print(last_digit, end=" ")
     print("and is greater than 5")
-elif number % 10 == 0:
+elif last_digit == 0:
     print("Last digit of", end=" ")
     print(number, end=" ")
     print("is", end=" ")
-    print(number % 10, end=" ")
+    print(last_digit, end=" ")
     print("and is 0")
 elif (number % 10 < 6) and (number % 10 != 0):
     print("Last digit of", end=" ")
     print(number, end=" ")
     print("is", end=" ")
-    print(number % 10, end=" ")
+    print(last_digit, end=" ")
     print("and is less than 6 and not 0")
