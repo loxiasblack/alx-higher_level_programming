089a8b19c1a6120419296d5b070a6153a88f404a
ilias <iliaschokairi@gmail.com>
2023-10-03 11:53:53 +0100

    print alphabet with python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 for a in alphabet:
-    print(a, end="")
+    print("{}".format(a), end="")
