d10693128f953293b1a9c4ed24668c4a81460267
ilias <iliaschokairi@gmail.com>
2023-10-04 11:37:59 +0100

    check if cycle exist in my linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - check if the linked list
+ * has a cycle
+ * @list: node to search in
+ * Return: 1 or 0
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *head;
 
+	head = list;
+	while (list->next != NULL)
+	{
+		if (list->next == head)
+		{
+			return (1);
+		}
+		list = list->next;
+	}
+	return (0);
+}
