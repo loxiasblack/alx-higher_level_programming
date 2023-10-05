09aa9c48ba5597f75634ac370efa868573c2efb4
ilias <iliaschokairi@gmail.com>
2023-10-05 13:59:58 +0100

    import a code crypted

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-        for name in dir(hidden_4):
+    import hidden_4
+    for name in dir(hidden_4):
+        if not(name[:2] == "__"):
             print(name)
