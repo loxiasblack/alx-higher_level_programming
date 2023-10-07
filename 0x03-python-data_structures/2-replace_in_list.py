20e824d96d232aa5d6aaccbf7843a3c97829465f
ilias <iliaschokairi@gmail.com>
2023-10-07 10:17:48 +0100

    replace element in index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return my_list
+        new_list = list.copy(my_list)
+        return new_list
     elif idx >= len(my_list):
-        return my_list
+        new_list = list.copy(my_list)
+        return new_list
     else:
         new_list = list.copy(my_list)
         new_list[idx] = element
