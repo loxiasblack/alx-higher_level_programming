21f6324523413e4742db3b3ea86956475b3de627
ilias <iliaschokairi@gmail.com>
2023-10-07 23:25:01 +0100

    forget none issue

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return (0, None)
     for i in range(len(sentence)):
         i += 1
     return (i, sentence[0])
