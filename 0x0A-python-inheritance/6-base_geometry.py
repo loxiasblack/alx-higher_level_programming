95f40d409e2a848fc24055eefb9e7141773d6af3
ilias <iliaschokairi@gmail.com>
2023-11-06 17:09:26 +0100

    raise exception message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Improve Geometry"""
+
+
+class BaseGeometry:
+
+    def __init__(slef):
+        """init istance"""
+        pass
+
+    def area(self):
+        """raise an exepction with the
+        follow message"""
+        raise Exception("area() is not implemented")
