decc8c011e58b4676410d96011b66417e1c8bab1
ilias <iliaschokairi@gmail.com>
2023-11-07 20:11:16 +0100

    overwrite file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""write text in file"""
+
+
+def write_file(filename="", text=""):
+    """function that allow to write a text and return the """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
+        return (f.write(text))
