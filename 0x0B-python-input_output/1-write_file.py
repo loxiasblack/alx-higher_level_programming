ac1af3039eb01b5e991d5196082bf58ca33ae99b
ilias <iliaschokairi@gmail.com>
2023-11-07 21:15:12 +0100

    push to overwrit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """function that allow to write a text and return the """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
         return (f.write(text))
