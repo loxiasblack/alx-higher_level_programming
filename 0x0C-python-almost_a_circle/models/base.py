bfccf28597693236890c2c2e97e9dcce74dff9d4
ilias <iliaschokairi@gmail.com>
2023-12-19 17:07:36 +0100

    creat id

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""!.base class module"""
+import json
+
+
+class Base:
+    """class for the object"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """instantiation with id"""
+        if id is not None:
+            self.id = id
+        else:
+            Base._nb_objects =+ 1
+            self.id = Base._nb_objects
+
