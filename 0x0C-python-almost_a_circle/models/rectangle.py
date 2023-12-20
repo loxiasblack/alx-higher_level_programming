6246ce6a793958f24aefdf0ce7a9441476bf13eb
ilias <iliaschokairi@gmail.com>
2023-12-20 08:12:00 -0800

    creat a method area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """method that return area of rectangle"""
+        return self.__height * self.__width
