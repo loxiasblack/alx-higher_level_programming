6f55279ea37e0acb14002fdeff15763a3e7d3182
ilias <iliaschokairi@gmail.com>
2023-12-20 08:44:51 -0800

    add __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,7 @@ class Rectangle(Base):
         """method that display rectangle with #"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
+                f" - {self.__width}/{self.__height}")
