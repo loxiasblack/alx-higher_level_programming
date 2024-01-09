f5831b2b5d7561a8dd9fe732787b7a7ada32849c
ilias <iliaschokairi@gmail.com>
2024-01-09 13:43:06 +0100

    count the occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce(function (count, currentElement) {
+    return (currentElement === searchElement) ? count + 1 : count;
+  }, 0);
+};
