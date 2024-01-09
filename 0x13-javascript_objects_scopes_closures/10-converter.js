fbbf777ce897c7ef726e4ce6b9ac150589b81d74
ilias <iliaschokairi@gmail.com>
2024-01-09 14:08:39 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-    return function (arg) {
-      return arg.toString(base);
-    };
+  return function (arg) {
+    return arg.toString(base);
   };
+};
