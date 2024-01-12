413b1cf2821964abd8dc0c440002af50b912a2e2
ilias <iliaschokairi@gmail.com>
2024-01-12 20:29:23 +0100

    just for test

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Scripts that lists all cities from databases"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT DISTINCT * FROM cities,states ORDER BY cities.id ASC")
+    for city in cursor.fetchall():
+        print(city)
+    cursor.close()
+    db.close()
