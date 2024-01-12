dfdb14bf2300c19f473e2f9e27819c1aa8eeed62
ilias <iliaschokairi@gmail.com>
2024-01-12 12:36:43 +0100

    try an other time

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""script that lists all states from the database """
-
-import sys
+"""scripts that list all states from database"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-        )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    for state in cursor.fetchall():
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for state in cur.fetchall():
         print(state)
-    cursor.close()
+        
+    cur.close()
     db.close()
