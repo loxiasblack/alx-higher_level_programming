86547c34bde5fbb32b66928fd7e2946e739a3b27
ilias <iliaschokairi@gmail.com>
2024-01-12 17:11:18 +0100

    what is lacking in my program

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,16 +5,23 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    user=sys.argv[1]
+    passwd=sys.argv[2]
+    dbname=sys.argv[3]
+    statename=sys.argv[4]
+    
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
+        user=user,
+        passwd=passwd,
+        db=dbname
     )
-    searchname = sys.argv[4]
+    
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (searchname,))
+    cursor.execute(query, (statename,))
     for name in cursor.fetchall():
         print(name)
     cursor.close()
