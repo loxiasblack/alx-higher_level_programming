1ec7e1bc2b2a3249d105a5f2fb49bfd43b57b4da
ilias <iliaschokairi@gmail.com>
2024-01-12 15:38:37 +0100

    case sensetive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%'")
     for state in cursor.fetchall():
         print(state)
 
