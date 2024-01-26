bd011544b0a43295a5c7a4d567f2512974aa311f
ilias <iliaschokairi@gmail.com>
2024-01-26 21:09:44 +0100

    ftching with requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""scripts that fetch with requests package"""
+
+import requests
+
+if __name__ == "__main__":
+    
+    s  = requests.get("https://alx-intranet.hbtn.io/status")
+    d = s.content.decode("utf-8")
+    print("Body response:")
+    print("\t- type: {}".format(type(d)))
+    print("\t- content: {}".format(d))
+    
---
+++ 0x11-python-network_1/s.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""fetche with request module from an url"""
+
+import requests
+
+if __name__ == "__main__":
+    
