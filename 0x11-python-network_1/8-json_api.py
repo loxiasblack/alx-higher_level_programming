3cd5f830425715e0637b3a446e5cce8ca3fac86e
ilias <iliaschokairi@gmail.com>
2024-01-26 23:46:38 +0100

    try my prtotype

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-"""send POST request with a letter as a parameter"""
+"""Sends a POST request with email as a parameter"""
 
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    if sys.argv[2] == None:
-        sys.argv = ""
-    payload = {'q': sys.argv[2]}
-    r = requests.post(sys.argv[1], params=payload)
-    print(r.text)
+    query_param = sys.argv[1]
+    
+    if len(sys.argv) > 1:
+        query_param = sys.argv[1]
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': sys.argv[1]})
+    text = r.content.decode("utf-8")
+    if not (text.startswith("{") and text.endswith("}\n")):
+        print("Not a valid JSON")
+    parts = text.strip("{'}\n").split(",")
+    if not parts:
+        print("No result")
+    else:
+        data = {}
+        for part in parts:
+            key, value = part.split(":")
+            key = key.strip("'\"")
+            value = value.strip("'\"")
+            data[key] = value
+        print("[{}] {}".format(data["id"], data["name"]))
