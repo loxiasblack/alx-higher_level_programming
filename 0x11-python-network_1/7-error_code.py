ac92281adf50288371651f9940e1928bb082fa95
ilias <iliaschokairi@gmail.com>
2024-01-26 21:50:19 +0100

    raise error or diplay the text

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""send requests to the URL and display the body response
+if the code_status is more than 400"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code > 400:
+        print(r.status_code)
+    else:
+        print(r.text)
