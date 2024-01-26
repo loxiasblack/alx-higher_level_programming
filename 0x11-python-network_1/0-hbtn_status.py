f4c1293dcd98724e9cd9a5b23e13728e0477720d
ilias <iliaschokairi@gmail.com>
2024-01-26 01:15:45 +0100

    what is my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that fetches the URL given and displayed the body"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("  - type: {}".format(type(body)))
+    print("  - content: {}".format(body))
+    print("  - utf8 content: {}".format(body.decode('utf-8')))
