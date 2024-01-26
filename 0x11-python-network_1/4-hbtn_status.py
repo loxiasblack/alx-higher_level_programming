672c7691d05ab7abeecd5debb65767f087a81643
ilias <iliaschokairi@gmail.com>
2024-01-26 21:10:59 +0100

    fix pycodestyle

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,10 +4,9 @@
 import requests
 
 if __name__ == "__main__":
-    
-    s  = requests.get("https://alx-intranet.hbtn.io/status")
+
+    s = requests.get("https://alx-intranet.hbtn.io/status")
     d = s.content.decode("utf-8")
     print("Body response:")
     print("\t- type: {}".format(type(d)))
     print("\t- content: {}".format(d))
-    
