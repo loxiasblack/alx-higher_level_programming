b8e91ee91e70bfb20d49ed9fd30727417996b2e1
ilias <iliaschokairi@gmail.com>
2024-02-21 01:04:06 +0100

    display in the right order

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,24 +7,7 @@ request.get(process.argv[2], function (error, response) {
     console.error(error);
   } else {
     let count = 0;
-    const obj = JSON.parse(response.body);
-    for (let key in obj) {
-      if (key === 'results') {
-        const data = obj[key];
-        for (let index = 0; index < data.length; index++) {
-          for (key in data[index]) {
-            if (key === 'characters') {
-              const characters = data[index][key];
-              for (let index = 0; index < characters.length; index++) {
-                if (characters[index] === 'https://swapi-api.alx-tools.com/api/people/18/') {
-                  count++;
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    console.log(count);
+    const obj = JSON.parse(body);
+    console.log(obj)
   }
 });
