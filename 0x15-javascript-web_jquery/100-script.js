d57d71c8f3ab5b0d06b9c554c65a0d934da6c9b9
ilias <iliaschokairi@gmail.com>
2024-03-06 12:12:18 +0100

    delete javascript-web_jquery

--- 0x15-javascript-web_jquery/100-script.js
+++
@@ -1,1 +0,0 @@
-document.querySelector('header').style.cssText = '#FF0000';
--- 0x15-javascript-web_jquery/4-script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#toggle_header').click(function() {
-        if ($('header').hasClass('red')) {
-            $('header').removeClass('red').addClass('green');
-        } else {
-            $('header').removeClass('green').addClass('red');
-        }
-    });
-});
