38f882d181097154d66569fa9a07f97e782221dc
ilias <iliaschokairi@gmail.com>
2024-03-15 15:33:59 +0000

    create an algho that print the second largest in the array of the args

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
-function findsecondlargest (arr) {
-  let largest = arr[2];
-  let secondLargest = -Infinity;
-  for (let i = 3; i < arr.length; i++) {
-    if (arr[i] > largest) {
-      secondLargest = largest;
-      largest = arr[i];
-    } else if (arr[i] < largest && arr[i] > secondLargest) {
-      secondLargest = arr[i];
-    }
-  }
-  return secondLargest;
-}
-const arg = process.argv;
-if (arg.length < 4) {
+const process = require('process');
+if (process.argv.length < 3) {
   console.log(0);
-} else {
-  console.log(findsecondlargest(arg));
+} if (process.argv.length > 2) {
+  const array = [];
+  let x = 0;
+  let i = 0;
+  for (let index = 2; index < process.argv.length; index++) {
+    x = parseInt(process.argv[index]);
+    array.push(x);
+  }
+  array.sort(function (a, b) { return a - b; });
+  while (i < array.length) {
+    if (i === array.length - 2) {
+      console.log(array[i]);
+      break;
+    }
+    i++;
+  }
 }
