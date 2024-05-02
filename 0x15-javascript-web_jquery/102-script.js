9c0e23e0c19adb4a47d04283ac731920aaf89b0b
Ilias Chokairi <iliaschokairi@gmail.com>
2024-05-02 22:04:20 +0100

    102.script

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#btn_translate').click(function () {
+      const code = $('#language_code').val();
+      $.get(`https://hellosalut.stefanbohacek.dev/?lang=${code}`, function (data) {
+        $('#hello').text(data.hello);
+      });
+    });
+  });
